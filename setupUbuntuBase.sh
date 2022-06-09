a4db80372c9689d747c98ce9e3cc224bd4c4387d
Shaun <shaun_bowman@hotmail.com>
2022-06-09 12:21:36 -0400

    updates to setupUbuntuBase.sh

--- setupUbuntuBase.sh
+++ setupUbuntuBase.sh
@@ -7,6 +7,9 @@
 #
 
 # Setup OS level package manager
+tput setaf 1
+echo "------ Setting up Package Manager -------"
+tput sgr0
 sudo apt install software-properties-common
 sudo apt-add-repository universe
 sudo apt-add-repository multiverse
@@ -15,29 +18,93 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Install windowing system & vnc viewer. For linode.
-# sudo apt install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
-# sudo apt install tigervnc-standalone-server
+tput setaf 1
+read -p "Do u need to setup 'ubuntu-desktop' and vnc?" -n 1 -r
+tput sgr0
+echo # create newline, optional
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	tput setaf 1
+	echo "installing ubuntu-desktop!"
+	tput sgr0
+	sudo apt install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+	tput setaf 1
+	echo "installing vnc!"
+	tput sgr0
+	sudo apt install tigervnc-standalone-server
+fi
 
 # Install misc admistrative terminal apps
-sudo apt-get install mosh
+tput setaf 1
+echo "------ Installing Misc Utils  -------"
+tput sgr0
 sudo snap install btop
-sudo apt-get install vim
-sudo apt-get install git
+sudo apt install mosh
+sudo apt install vim
+sudo apt install git
 sudo apt install rdiff
 sudo apt install wget
 sudo apt install curl
 sudo apt install gnupg
 sudo apt install lsb-release
+sudo apt install dos2unix
+sudo apt install colordiff
+sudo apt install tree
+sudo apt install bat
+sudo apt install httpie
+sudo apt install tldr
+sudo apt install fzf
+tput setaf 1
+read -p "Do u need to setup 'fd' fast find?" -n 1 -r
+tput sgr0
+echo # create newline, optional
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	tput setaf 1
+	echo "installing command 'fd'!"
+	tput sgr0
+	sudo apt install fd-find
+	cd $HOME
+	echo 'alias fd="fdfind"' >> .bashrc
+fi
+sudo apt install ripgrep
+sudo apt install htop
+tput setaf 1
+read -p "Do u need to setup 'z' jump-around?" -n 1 -r
+tput sgr0
+echo # create newline, optional
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	tput setaf 1
+	echo "installing command 'z'!"
+	tput sgr0
+	cd $HOME
+	wget https://raw.githubusercontent.com/rupa/z/master/z.sh
+	chmod +x z.sh
+	echo '. ~/z.sh' >> .bashrc
+fi
 
 
 # Setup python for ML / AI / Datascience
 sudo apt install python3-pip
 sudo apt install python3-venv
 cd $HOME
-python3 -m venv venv38
-source venv38/bin/activate
-echo 'source venv38/bin/activate' >> .bashrc
-exec $SHELL
+tput setaf 1
+read -p "Do u need to setup python venv (venv38)f? " -n 1 -r
+tput sgr0
+echo # create newline, optional
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	tput setaf 1
+	echo "u said yes!"
+	tput sgr0
+	python3 -m venv venv38
+	source venv38/bin/activate
+	echo 'source venv38/bin/activate' >> .bashrc
+fi
+tput setaf 1
+echo "------ Setting up python modules -------"
+tput sgr0
 python3 -m pip install numpy
 python3 -m pip install pandas
 python3 -m pip install scipy
@@ -54,35 +121,61 @@ python3 -m pip install jupyter_http_over_ws
 python3 -m pip install googlemaps
 
 # Setup home folder
-cd $HOME
-mkdir git
-mkdir mlDatasets
-cd git
-git clone https://github.com/sbow/shadows
-git clone https://github.com/sbow/RpiDatalogger
-git clone https://github.com/sbow/pyCast
-git clone https://github.com/sbow/mowbotCpp
-git clone https://github.com/sbow/scratch
-git clone https://github.com/sbow/vim
-git clone https://github.com/sbow/coursera
-git clone https://github.com/sbow/udacity
-
-# Grab ML datasets
-cd $HOME
-cd mlDatasets
+tput setaf 1
+read -p "Do u want to setup home dirs/git clones?? " -n 1 -r
+tput sgr0
+echo # create newline, optional
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	tput setaf 1
+	echo "doing initial git clones and folder creation!"
+	tput sgr0
+	cd $HOME
+	mkdir git
+	mkdir mlDatasets
+	cd git
+	git clone https://github.com/sbow/shadows
+	git clone https://github.com/sbow/RpiDatalogger
+	git clone https://github.com/sbow/pyCast
+	git clone https://github.com/sbow/mowbotCpp
+	git clone https://github.com/sbow/scratch
+	git clone https://github.com/sbow/vim
+	git clone https://github.com/sbow/coursera
+	git clone https://github.com/sbow/udacity
+fi
 
 # Install ros
-echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add 
-sudo apt update
-sudo apt install ros-noetic-desktop-full
-sudo apt install locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update
-sudo apt upgrade
-sudo apt install ros-rolling-desktop
+tput setaf 1
+read -p "Do u need to install ros1?? " -n 1 -r
+tput sgr0
+echo # create newline, optional
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	tput setaf 1
+	echo "installing ros1!"
+	tput sgr0
+	echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
+	curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add 
+	sudo apt update
+	sudo apt install ros-noetic-desktop-full
+fi
+tput setaf 1
+read -p "Do u need to install ros2?? " -n 1 -r
+tput sgr0
+echo # create newline, optional
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	tput setaf 1
+	echo "installing ros2!"
+	tput sgr0
+	sudo apt install locales
+	sudo locale-gen en_US en_US.UTF-8
+	sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+	export LANG=en_US.UTF-8
+	sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+	sudo apt update
+	sudo apt upgrade
+	sudo apt install ros-rolling-desktop
+fi
 
